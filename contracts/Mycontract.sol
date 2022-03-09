4de2e63d63f7477d54cc473ad4a7951a7b75663d
"samuelshabach" <"opeyemishabach@gmail.com">
2022-03-09 16:03:15 +0100

    first commit

---
+++ contracts/Mycontract.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Mycontract {
+    string public name;
+    string public greetingPrefix = "Hello ";
+
+    constructor(string memory initialName){
+        name = initialName;
+    }
+
+    function setName(string memory newName) public {
+        name = newName;
+    }
+
+    function getGreeting() public view returns (string memory) {
+        return string(abi.encodePacked(greetingPrefix, name));
+    }
+}
